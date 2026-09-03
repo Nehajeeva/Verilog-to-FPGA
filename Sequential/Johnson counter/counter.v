7b5d001fa482de58e23557016aafb0d16da1c927
Neha <nehajeeva2106@gmail.com>
2026-09-03 10:15:50 +0530

    Add UUT Verilog design

---
+++ Sequential/Johnson counter/counter.v
@@ -0,0 +1,23 @@
+module JohnsonCounter(q, clk, reset);
+output [3:0] q;
+input clk, reset;
+reg q0, q1, q2, q3;
+always @(posedge clk or posedge reset)
+begin
+if (reset)
+begin
+q0 <= 0;
+q1 <= 0;
+q2 <= 0;
+q3 <= 0;
+end
+else
+begin
+q0 <= ~q3;
+q1 <= q0;
+q2 <= q1;
+q3 <= q2;
+end
+end
+assign q = {q3, q2, q1, q0};
+endmodule
